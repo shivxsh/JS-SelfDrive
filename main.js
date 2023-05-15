9a22868b9308b33a4e2e761b44b6508ae407ce27
SreeShivesh <sreeshiveshpsg@gmail.com>
2023-05-15 19:04:19 +0530

    Defining Road - Final

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const canvas = document.getElementById("myCanvas");
-canvas.width = 400;
+canvas.width = 200;
 
 
 //First step in building a car is to get the context
 
 const ctx = canvas.getContext("2d"); //getting the context
 const road = new Road(canvas.width/2, canvas.width*0.9);
-const car = new Car(200,200,30,50);  // creating a new object called car from car.js
+const car = new Car(road.getLaneCenter(2),100,30,50);  // creating a new object called car from car.js
 car.draw(ctx);
 
 
@@ -18,8 +18,14 @@ function animate(){
 
     canvas.height=window.innerHeight;
     
+    ctx.save();
+//To have a camera view pinned on the car such that it feels like the road is infinitely moving
+    ctx.translate(0,-car.y + canvas.height*0.8);
     road.draw(ctx);
     car.draw(ctx);
+    ctx.restore();
+
+
 //requestAnimationFrame calls the "animate()" method many times per second.
 //This gives the illusion of movement.
     requestAnimationFrame(animate)
--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road{
-    constructor(x,width,laneCount=3){
+    constructor(x,width,laneCount=4){
         this.x=x;
         this.width=width;
         this.laneCount = laneCount;
@@ -8,29 +8,51 @@ class Road{
         this.right = x + width/2;
 
         //Move our road infinitely upward
-        const infinity = 100000000; //Some large random value
+        const infinity = 10000000; //Some large random value
         this.top = -infinity;
         this.bottom = infinity; //Note that y axis increases as it goes down in a computer
+
+    //JS - objects
+    //i.e. topLeft.x = this.left and topRight.y= 
+        const topLeft = {x:this.left,y:this.top};
+        const topRight = {x:this.right,y:this.top};
+        const bottomLeft = {x:this.left,y:this.bottom};
+        const bottomRight = {x:this.right,y:this.bottom};
+        this.borders=[
+            [topLeft,bottomLeft],
+            [topRight,bottomRight]
+        ];
     }
 
+    getLaneCenter(laneIndex){ //Note laneIndex starts from 0 and its just a number given to each lane
+        const laneWidth = this.width/this.laneCount;
+        return this.left + laneWidth/2 + Math.min(laneIndex,this.laneCount-1)*laneWidth;
+    }
     draw(ctx){
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        for(let i=0; i<=this.laneCount;i++){
+        for(let i=1; i<=this.laneCount-1;i++){
         //Defined the "lerp" function inside the util.js file    
-            const x = lerp(this.left, this.right, i/this.laneCount);
-
-        // "i/laneCount" will return a value between 0 and 1. and all inbetween values will be percentage or fractions
-
+            const x = lerp(this.left, this.right, i/this.laneCount); // "i/laneCount" will return a value between 0 and 1. and all inbetween values will be percentage or fractions
+        
+            
+            ctx.setLineDash([20,20]);  //first param = length of dashed line; second = break between 2 lines
+            
+            
             ctx.beginPath();
             ctx.moveTo(x, this.top); // moves the path to the specified point in the canvas, without creating a line.
             ctx.lineTo(x,this.bottom); // adds another point to which a line can be drawn FROM the old point
             ctx.stroke(); // draws the line from the moveTo to the lineTo (Traces the points in order)
-    
         }
-        
-        
 
+        ctx.setLineDash([]);
+        //for each loop to go through the array borders
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x,border[0].y);
+            ctx.lineTo(border[1].x,border[1].y);
+            ctx.stroke();
+        });
     }
 }
