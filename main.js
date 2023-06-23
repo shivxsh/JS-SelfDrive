7d6b95d3f272240b140236d91e87366e1bc64e55
SreeShivesh <sreeshiveshpsg@gmail.com>
2023-06-23 14:22:09 +0530

    Adding Traffic

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ canvas.width = 200;
 const ctx = canvas.getContext("2d"); //getting the context
 const road = new Road(canvas.width/2, canvas.width*0.9);
 const car = new Car(road.getLaneCenter(2),100,30,50);  // creating a new object called car from car.js
+//Array of cars : traffic
+const traffic = [
+    new Car(road.getLaneCenter(1), -100,30,50),
+];
 
 
 
@@ -14,6 +18,10 @@ const car = new Car(road.getLaneCenter(2),100,30,50);  // creating a new object 
 animate();
 
 function animate(){
+
+    for(let i=0; i<traffic.length; i++){
+        traffic[i].update(road.borders);
+    }
     car.update(road.borders);
 
     canvas.height=window.innerHeight;
@@ -22,6 +30,9 @@ function animate(){
 //To have a camera view pinned on the car such that it feels like the road is infinitely moving
     ctx.translate(0,-car.y + canvas.height*0.8);
     road.draw(ctx);
+    for(let i=0; i<traffic.length; i++){
+        traffic[i].draw(ctx);
+    }
     car.draw(ctx);
     ctx.restore();
 
