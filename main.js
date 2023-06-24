85277fc074f1e6e86b16f8ee3dd9e8de48c79b8a
SreeShivesh <sreeshiveshpsg@gmail.com>
2023-06-24 20:24:11 +0530

    Parallelization

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const carCanvas = document.getElementById("carCanvas");
 carCanvas.width = 200;
+
 const networkCanvas = document.getElementById("networkCanvas");
 networkCanvas.width = 300;
 
@@ -9,7 +10,11 @@ networkCanvas.width = 300;
 const carCtx = carCanvas.getContext("2d"); //getting the context
 const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
-const car = new Car(road.getLaneCenter(2),100,30,50,"AI");  // creating a new object called car from car.js
+
+const N = 100; //100 cars generated
+const cars = generateCars(N);
+
+
 //Array of cars : traffic
 const traffic = [
     new Car(road.getLaneCenter(2), -100,30,50, "DUMMY",2),
@@ -20,33 +25,47 @@ const traffic = [
 //animate the car to move
 animate();
 
-function animate(){
 
-    function generateCars(N){
-        
+function generateCars(N){
+    const cars=[];
+    for(let i=1; i<=N; i++){
+        cars.push(new Car(road.getLaneCenter(1),100,30,50, "AI"));
     }
+    return cars;
+}
+
+
+function animate(){
 
     for(let i=0; i<traffic.length; i++){
         traffic[i].update(road.borders,[]);  //Empty array so the traffic cars wont interact with itself and get damaged.
     }
-    car.update(road.borders, traffic);
+
+    for(let i=0; i<cars.length; i++){
+        cars[i].update(road.borders, traffic);
+    }
 
     carCanvas.height=window.innerHeight;
     networkCanvas.height=window.innerHeight;
     
     carCtx.save();
 //To have a camera view pinned on the car such that it feels like the road is infinitely moving
-    carCtx.translate(0,-car.y + carCanvas.height*0.8);
+    carCtx.translate(0,-cars[0].y + carCanvas.height*0.8);
+
+
     road.draw(carCtx);
     for(let i=0; i<traffic.length; i++){
         traffic[i].draw(carCtx,"red");
     }
-    car.draw(carCtx, "blue");
+
+    for(let i=0; i<cars.length; i++){
+        cars[i].draw(carCtx, "blue");
+    }
 
     carCtx.restore();
 
     
-    Visualizer.drawNetwork(networkCtx, car.brain);
+    Visualizer.drawNetwork(networkCtx, cars[0].brain);
     
 
 //requestAnimationFrame calls the "animate()" method many times per second.
--- network.js
+++ network.js
@@ -1,20 +1,18 @@
 class NeuralNetwork{
-    constructor(neuronCounts){ //count of neurons
-        this.levels = [];
-        for(let i=0; i<neuronCounts.length; i++){
+    constructor(neuronCounts){
+        this.levels=[];
+        for(let i=0;i<neuronCounts.length-1;i++){
             this.levels.push(new Level(
-                neuronCounts[i], //input neurons
-                neuronCounts[i+1] //output neurons
+                neuronCounts[i],neuronCounts[i+1]
             ));
         }
     }
 
-    static feedForward(givenInputs, network){
-    //Calling levels to produce its output
-        let outputs = Level.feedForward(givenInputs, network.levels[0]);
-    //Loop through the remaining levels to update the output with feedForward result from level i
-        for(let i=1; i<network.levels.length; i++){
-            outputs = Level.feedForward(
+    static feedForward(givenInputs,network){
+        let outputs=Level.feedForward(
+            givenInputs,network.levels[0]);
+        for(let i=1;i<network.levels.length;i++){
+            outputs=Level.feedForward(
                 outputs,network.levels[i]);
         }
         return outputs;
@@ -22,53 +20,49 @@ class NeuralNetwork{
 }
 
 class Level{
-    constructor(inputCount, outputCount){
-        //Just what u learnt in ML last sem. Input, output and bias
-        this.inputs = new Array(inputCount);
-        this.outputs = new Array(outputCount);
-        this.biases = new Array(outputCount);
+    constructor(inputCount,outputCount){
+        this.inputs=new Array(inputCount);
+        this.outputs=new Array(outputCount);
+        this.biases=new Array(outputCount);
 
-        this.weights = [];
-        for(let i=0; i<inputCount; i++){
-            this.weights[i] = new Array(outputCount);
+        this.weights=[];
+        for(let i=0;i<inputCount;i++){
+            this.weights[i]=new Array(outputCount);
         }
 
-        //Randomize the weights and biases initially.
         Level.#randomize(this);
     }
 
     static #randomize(level){
-        for(let i=0; i<level.inputs.length; i++){
-            for(let j=0; j<level.outputs.length; j++){
-                level.weights[i][j] = Math.random()*2 - 1;
+        for(let i=0;i<level.inputs.length;i++){
+            for(let j=0;j<level.outputs.length;j++){
+                level.weights[i][j]=Math.random()*2-1;
             }
         }
 
-        for(let i=0; i<level.biases.length; i++){
-            level.biases[i] = Math.random() * 2 -1;
+        for(let i=0;i<level.biases.length;i++){
+            level.biases[i]=Math.random()*2-1;
         }
     }
 
-    static feedForward(givenInputs, level){
-        for(let i=0; i<level.inputs.length; i++){
-            level.inputs[i] = givenInputs[i];
+    static feedForward(givenInputs,level){
+        for(let i=0;i<level.inputs.length;i++){
+            level.inputs[i]=givenInputs[i];
         }
 
-        for(let i=0; i<level.outputs.length; i++){
-            let sum = 0; //sum between value of inputs and weights
-            for(let j=0; j<level.inputs.length; j++){
-                sum += level.inputs[j] * level.weights[j][i]; //Normal NN finding of value of a node. weight * input
+        for(let i=0;i<level.outputs.length;i++){
+            let sum=0
+            for(let j=0;j<level.inputs.length;j++){
+                sum+=level.inputs[j]*level.weights[j][i];
             }
 
-            if(sum > level.biases[i]){
-                level.outputs[i] = 1;  //output neuron is On
-            }
-            else{
-                level.outputs[i] = 0; //output neuron is off
-            }
+            if(sum>level.biases[i]){
+                level.outputs[i]=1;
+            }else{
+                level.outputs[i]=0;
+            } 
         }
 
         return level.outputs;
     }
 }
-
